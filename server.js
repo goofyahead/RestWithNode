a30f21a80a0d0b6679ae7b9c152fb6b444e2b64f
goofyahead <goofyahead@goofyahead-VirtualBox.(none)>
2012-12-20 07:09:32 +0100

    added mongoose support for mongoDB, schema pending

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ var express = require('express');
 var app = express();
 var dishes = require('./routes/dishes');
 
+//GET REQUESTS
 app.get('/dishes', dishes.findAll);
-
 app.get('/dishes/:id', dishes.findById);
+//PUT REQUEST
+app.put('/dishes/:id', dishes.updateDish);
+//POST REQUEST
+app.post('/dishes', dishes.addDish);
+//DELETE REQUEST
+app.delete('/dishes/:id', dishes.deleteDish);
 
 app.listen(3000);
-console.log('Listening on port 3000');
+console.log('Listening on port 3000...');
